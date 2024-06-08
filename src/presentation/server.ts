5f055c795df7ec637da96bdd9b3dc45b1c4acc78
Alonso <marenas@itsmarts.com.mx>
2024-06-08 10:48:49 -0600

    Fin de la sección 8

---
+++ src/presentation/server.ts
@@ -0,0 +1,26 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+
+export class Server {
+
+    public static start() {
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/5 * * * * *', // cronTime
+            () => {
+                const url = `http://localhost:3000`
+                new CheckService(
+                    () => console.log(`${url} is ok`),
+                    ( error ) => console.log(error),
+                    ).execute(url);
+                // new CheckService().execute(`http://google.com`);
+
+            }
+        );
+
+    
+    }
+
+}
