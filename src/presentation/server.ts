fd2597c3afe8270e779e44d1150ba45b2d7b4a97
Alonso <marenas@itsmarts.com.mx>
2024-06-10 11:29:04 -0600

    Fin sección 9, ENV

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl( 
+    new FileSystemDatasource(), 
+ )
 
 
 export class Server {
@@ -10,8 +16,9 @@ export class Server {
         CronService.createJob(
             '*/5 * * * * *', // cronTime
             () => {
-                const url = `http://localhost:3000`
+                const url = `http://google.com`
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log(`${url} is ok`),
                     ( error ) => console.log(error),
                     ).execute(url);
